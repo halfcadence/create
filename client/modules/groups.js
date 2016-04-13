85c4bf4460da62ab4bd4942966f519cf60467db8
Andrew <andrew-s@comcast.net>
2016-04-13 01:34:13 -0400

    veil understands click events

--- client/modules/groups.js
+++ client/modules/groups.js
@@ -8,33 +8,45 @@ Template.application.events({ //events on the page body
   }
 });
 
-//remove when used
-//$(document).one( "click", function() { alert("You'll only see this once!"); } );
-
 let showTrashCards = function() {
   // TODO: increase efficiency of query by adding {locationX: 1, locationY: 1}
   // TODO: sort with find.sort and display in order
   let trashCards = Cards.find({groupId: "trash"});
-  displayVeil();
+  showVeil();
   drawPositions = makePlacementArray();
   let drawPositionIndex = 0;
   trashCards.forEach(function(card) {
       Modules.client.moveThing(document.getElementById(card._id), drawPositions[drawPositionIndex].x,drawPositions[drawPositionIndex].y);
       drawPositionIndex = (drawPositionIndex + 1 ) % drawPositions.length;
+      Modules.client.setZIndex(document.getElementById(card._id), 10000001)
       console.log("locX" + card.locationX);
   });
 
 }
 
-let displayVeil = function () {
+//shows the veil
+//it removes itself when clicked upon
+let showVeil = function () {
   let veil = $(".veil");
+  let x = $(".x");
   veil.css('visibility', "visible");
+  $(x).on( "click.veil", hideVeil); //turn on click events under validator namespace
+  $(".pep").on( "mousedown.veil", hideVeil);
+}
+
+//hide the veil
+//remove its click listeners
+let hideVeil = function () {
+  let veil = $(".veil");
+  veil.css('visibility', "hidden");
+  $( ".x" ).off( ".veil" );
+  $(".pep").off( ".veil");
 }
 
 //TODO: dynamically allocate this array
 //makes the array with positions cards will be drawn from the group
 let makePlacementArray = function() {
-  let cornerDistance = 20;
+  let cornerDistance = 25;
   let xStep = 510; //distance between each card <->
   let yStep = 310; //distance between each card ^-v
   let drawPositions = [
--- client/modules/sharedFunctions.js
+++ client/modules/sharedFunctions.js
@@ -5,4 +5,11 @@ let moveThing = function(thing,x,y){
    position.top = y;
    $(thing).offset(position);
 };
+
+//sets the z index... TODO: refactor so z index is consistent across clients
+let setZIndex = function(thing, index){
+   $(thing).css('z-index', index);
+};
+
+Modules.client.setZIndex = setZIndex;
 Modules.client.moveThing = moveThing;
