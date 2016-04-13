95534c11654eb42e2998670558741898ce3ff48a
Andrew <andrew-s@comcast.net>
2016-04-13 00:20:54 -0400

    veil

--- client/modules/groups.js
+++ client/modules/groups.js
@@ -1,32 +1,21 @@
 Template.application.events({ //events on the page body
   "click button.group": function (event) { //click on new button
     event.preventDefault(); // Prevent default browser form submit
-    makeNewNoteCard();
   },
   "click button.red": function (event) { //click on new button
     event.preventDefault(); // Prevent default browser form submit
     showTrashCards();
   }
 });
-let makeNewNoteCard = function() {
-  var id = Cards.insert({
-        projectId: Modules.client.getProjectId(),
-        title: "",
-        body: "",
-        body2: "",
-        locationX:  195,
-        locationY: 25,
-        cost: "",
-        priority: "",
-        position: "left"
-  });
-}
+
+//remove when used
+//$(document).one( "click", function() { alert("You'll only see this once!"); } );
 
 let showTrashCards = function() {
   // TODO: increase efficiency of query by adding {locationX: 1, locationY: 1}
   // TODO: sort with find.sort and display in order
   let trashCards = Cards.find({groupId: "trash"});
-
+  displayVeil();
   drawPositions = makePlacementArray();
   let drawPositionIndex = 0;
   trashCards.forEach(function(card) {
@@ -37,10 +26,15 @@ let showTrashCards = function() {
 
 }
 
+let displayVeil = function () {
+  let veil = $(".veil");
+  veil.css('visibility', "visible");
+}
+
 //TODO: dynamically allocate this array
 //makes the array with positions cards will be drawn from the group
 let makePlacementArray = function() {
-  let cornerDistance = 10;
+  let cornerDistance = 20;
   let xStep = 510; //distance between each card <->
   let yStep = 310; //distance between each card ^-v
   let drawPositions = [
